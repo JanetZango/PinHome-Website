9842ab2d2e0a5fb1d83ac6d04274421a0bc90a3c
Leonard <spazgrafficz@outlook.com>
2019-01-18 07:35:36 +0200

    changes made

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -4,6 +4,7 @@ import {AngularFireDatabase, AngularFireList} from 'angularfire2/database';
 import { Observable } from 'rxjs';
 
 import {Router} from'@angular/router';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 
 
 @Component({
@@ -14,6 +15,8 @@ import {Router} from'@angular/router';
 export class SignInComponent implements OnInit {
   results;  
   userId;
+  message;
+
   constructor(private authen : AngularFireAuth, private db: AngularFireDatabase, public router: Router) { }
 
   ngOnInit() {
@@ -21,27 +24,84 @@ export class SignInComponent implements OnInit {
        
   }
 
+  goToSignUp(){
+    this.router.navigate(['/sign-up'])
+  }
+
 
   login(email,password){
-      this.results = this.authen.auth.signInWithEmailAndPassword(email,password).then(()=>{
+    let myAlert = document.getElementsByClassName("customAlert0") as HTMLCollectionOf <HTMLElement>;
+    let theOK = document.getElementById("theOkay" );
+    let leader = document.getElementsByClassName("loading") as HTMLCollectionOf <HTMLElement>
+    let b = window.innerHeight;
+
+    myAlert[0].style.top = (b / 3.5) + "px";
+    myAlert[0].style.left = "50%";
+    myAlert[0].style.transform = "translateX(-54%)"
+    if(email == "" || email == null){
+      this.message = "Please insert your email";
+      theOK.style.display = "block";
+      leader[0].style.display = "none";
+    }
+    else if(password == "" || password == null){
+      this.message = "Please insert your password";
+      theOK.style.display = "block";
+      leader[0].style.display = "none";
+    }
+    else{
+ 
+        this.message = "Loading...";
+        theOK.style.display = "none";
+        leader[0].style.display = "block"
+
+        this.results = this.authen.auth.signInWithEmailAndPassword(email,password).then(()=>{
          this.results = this.authen.authState.subscribe(data =>{
           this.userId =  data.uid;
-          // accpt(data)
-          alert('You have successfully logged in')
-          console.log(data);
+          myAlert[0].style.top = (b/3.5) + "px";
+          myAlert[0].style.left = "2.3%"; 
+          
            this.router.navigate(['/adding-data'])
            })
           }, Error =>{
-            alert(Error.message)
+
+            this.message = Error.message
+            theOK.style.display = "block";
+            leader[0].style.display = "none";
           })
+    }
+
+  }
+  
+  dismissAlert() {
+    let alerter = document.getElementsByClassName('customAlert0') as HTMLCollectionOf<HTMLElement>;
+    alerter[0].style.left = "-100%";
+    this.message = "" 
   }
 
-  forgotpassword(email: string){
+  forgotpassword(email){
+
+    
+
+    let myAlert = document.getElementsByClassName("customAlert0") as HTMLCollectionOf <HTMLElement>;
+    let leader = document.getElementsByClassName("loading") as HTMLCollectionOf <HTMLElement>
+    let theOK = document.getElementById("theOkay" );
+
+    myAlert[0].style.left = "25%";
+    theOK.style.display = "none";
+    leader[0].style.display = "block";
+
+    this.message = "Loading"
     return new  Promise<void>((resolve, reject)=>{
       this.authen.auth.sendPasswordResetEmail(email).then(()=>{
-     alert("Check your email")
+        myAlert[0].style.left = "25%"
+        this.message = "We have sent you a link to reset your password, check your email."
+        theOK.style.display = "block";
+        leader[0].style.display = "none";
       }, Error =>{
-        alert("Something went wrong.")
+        myAlert[0].style.left = "25%"
+        this.message = Error.message
+        theOK.style.display = "block";
+        leader[0].style.display = "none";
       });
     
     })
