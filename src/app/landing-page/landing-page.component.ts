413492f3bc84530d8bbd34041232b1c91138b0ea
Leonard <spazgrafficz@outlook.com>
2019-01-29 14:12:51 +0200

    Changes to the landing page

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import {AngularFireAuth} from 'angularfire2/auth';
 import {SlideshowModule} from 'ng-simple-slideshow';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
@@ -13,9 +15,31 @@ export class LandingPageComponent implements OnInit {
   "../../assets/imgs/dummy data/4.JPG", "../../assets/imgs/dummy data/5.JPG"
 ];
 
-  constructor(private slide: SlideshowModule) { }
+
+
+  constructor(private slide: SlideshowModule, private authState: AngularFireAuth, public router: Router) { }
 
   ngOnInit() {
+  }
+
+  getStarted(){
+    // this decides if you are online or not
+
+
+    this.authState.auth.onAuthStateChanged(user =>{
+      console.log(user)
+      if (user){
+
+        this.router.navigate(['/adding-data']);
+      }
+      else{
+        console.log('no user')
+
+        this.router.navigate(['/sign-in']);
+
+      }
+     });
+
   }
 
 }
